e1ea556ddd92a3bfc529be817051be44574101eb
wagnermateus <wagnermateus7@gmail.com>
2023-04-30 14:40:43 +0100

    Created the SignUp screen

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import {
 import { Loading } from "@components/Loading";
 import { THEME } from "./src/theme";
 import { SignIn } from "@screens/SignIn";
+import { SignUp } from "@screens/SignUp";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -18,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <SignUp /> : <Loading />}
     </NativeBaseProvider>
   );
 }
