c7bccfc20c86776b915bbca835305e70d2c3db40
wagnermateus <wagnermateus7@gmail.com>
2023-04-30 14:01:45 +0100

    Input component

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,11 +1,12 @@
-import { VStack, Image, Text, Center } from "native-base";
+import { VStack, Image, Text, Center, Heading } from "native-base";
 
 import BackgroundImg from "@assets/background.png";
 import LogoSvg from "@assets/logo.svg";
+import { Input } from "@components/Input";
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg="gray.700">
+    <VStack flex={1} bg="gray.700" px={10}>
       <Image
         source={BackgroundImg}
         alt="Pessoas treinando"
@@ -15,9 +16,20 @@ export function SignIn() {
       <Center my={24}>
         <LogoSvg />
         <Text color="gray.100" fontSize="sm">
-          {" "}
           Treine sua mente e o seu corpo.
         </Text>
+      </Center>
+      <Center>
+        <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+          Acesse a conta
+        </Heading>
+
+        <Input
+          placeholder="E-mail"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <Input placeholder="Senha" secureTextEntry />
       </Center>
     </VStack>
   );
