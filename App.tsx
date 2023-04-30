cfe87b66a61fa674b9f439ce7868790bdc501cf8
wagnermateus <wagnermateus7@gmail.com>
2023-04-30 13:29:18 +0100

    created SigniIn component and added the background image

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StatusBar, View } from "react-native";
+import { StatusBar } from "react-native";
 import { NativeBaseProvider } from "native-base";
 import {
   useFonts,
@@ -6,7 +6,8 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 import { Loading } from "@components/Loading";
-import { THEME } from "src/theme";
+import { THEME } from "./src/theme";
+import { SignIn } from "@screens/SignIn";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -17,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <View /> : <Loading />}
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
