27a340effb6341015334fd9e1833bcd9abafa4e7
wagnermateus <wagnermateus7@gmail.com>
2023-05-01 13:02:42 +0100

    Group component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,15 @@
+import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { Center, Text, VStack } from "native-base";
+import { HStack, VStack } from "native-base";
 
 export function Home() {
   return (
     <VStack flex={1}>
       <HomeHeader />
+      <HStack>
+        <Group name="Costa" />
+        <Group name="ombro" />
+      </HStack>
     </VStack>
   );
 }
