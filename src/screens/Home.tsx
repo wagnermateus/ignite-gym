c82818a00234ef208239aa5ca5385a0a028aebd5
wagnermateus <wagnermateus7@gmail.com>
2023-05-02 12:29:59 +0100

    Exercise Card Component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { ExerciseCard } from "@components/ExerciseCard";
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
 import { FlatList, HStack, Heading, VStack, Text } from "native-base";
@@ -44,6 +45,9 @@ export function Home() {
             4
           </Text>
         </HStack>
+
+        <ExerciseCard />
+        <ExerciseCard />
       </VStack>
     </VStack>
   );
