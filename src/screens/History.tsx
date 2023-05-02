b4072d12dbe3516b73ffd0b1b2ba16d066b6bc0f
wagnermateus <wagnermateus7@gmail.com>
2023-05-02 13:52:49 +0100

    HistoryCard component

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,10 +1,14 @@
+import { HistoryCard } from "@components/HistoryCard";
 import { ScreenHeader } from "@components/ScreenHeader";
-import { Center, Text, VStack } from "native-base";
+import { VStack } from "native-base";
 
 export function History() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico" />
+      <HistoryCard />
+      <HistoryCard />
+      <HistoryCard />
     </VStack>
   );
 }
