a1871438037d156d9e41557297f059aebe41b65a
wagnermateus <wagnermateus7@gmail.com>
2023-05-02 15:36:45 +0100

    Profile Screen-Skeleton Effect

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,18 +1,31 @@
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
-import { Center, ScrollView, VStack } from "native-base";
+import { Center, ScrollView, Skeleton, VStack } from "native-base";
+import { useState } from "react";
 
 export function Profile() {
+  const [photoIsLoaded, setPhotoIsLoaded] = useState(true);
+  const PHOTO_SIZE = 33;
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
       <ScrollView>
         <Center mt={6} px={10}>
-          <UserPhoto
-            source={{ uri: "https://github.com/wagnermateus.png" }}
-            alt="Foto do usuário"
-            size={33}
+          <Skeleton
+            w={PHOTO_SIZE}
+            h={PHOTO_SIZE}
+            rounded="full"
+            startColor="gray.500"
+            endColor="gray.400"
+            isLoaded={photoIsLoaded}
           />
+          {photoIsLoaded && (
+            <UserPhoto
+              source={{ uri: "https://github.com/wagnermateus.png" }}
+              alt="Foto do usuário"
+              size={33}
+            />
+          )}
         </Center>
       </ScrollView>
     </VStack>
