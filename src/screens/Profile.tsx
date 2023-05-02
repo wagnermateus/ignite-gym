003afbf13d0d6940313f0e37900cb26bc83e5016
wagnermateus <wagnermateus7@gmail.com>
2023-05-02 15:19:53 +0100

    Profile Screen - user photo

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,10 +1,20 @@
 import { ScreenHeader } from "@components/ScreenHeader";
-import { VStack } from "native-base";
+import { UserPhoto } from "@components/UserPhoto";
+import { Center, ScrollView, VStack } from "native-base";
 
 export function Profile() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
+      <ScrollView>
+        <Center mt={6} px={10}>
+          <UserPhoto
+            source={{ uri: "https://github.com/wagnermateus.png" }}
+            alt="Foto do usuário"
+            size={33}
+          />
+        </Center>
+      </ScrollView>
     </VStack>
   );
 }
