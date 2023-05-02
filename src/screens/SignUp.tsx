65ac589c73ea39fcd8d87622c11b47ab0e9194ad
wagnermateus <wagnermateus7@gmail.com>
2023-05-02 22:38:44 +0100

    SignUp-Getting the error message

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -15,7 +15,11 @@ type FormDataProps = {
 };
 
 export function SignUp() {
-  const { control, handleSubmit } = useForm<FormDataProps>();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormDataProps>();
   const navigation = useNavigation();
 
   function handleGoBack() {
@@ -63,7 +67,11 @@ export function SignUp() {
             render={({ field: { onChange, value } }) => (
               <Input placeholder="Nome" onChangeText={onChange} value={value} />
             )}
+            rules={{
+              required: "Informe o nome.",
+            }}
           />
+          <Text color="white">{errors.name?.message}</Text>
           <Controller
             control={control}
             name="email"
