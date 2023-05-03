37719b6bf01d1b9366aae394ba1ff697cddb1f69
wagnermateus <wagnermateus7@gmail.com>
2023-05-04 00:21:24 +0100

    feat: Creating account and logging in user

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -20,6 +20,8 @@ import { api } from "@services/api";
 import axios from "axios";
 import { Alert } from "react-native";
 import { AppError } from "@utils/AppError";
+import { useState } from "react";
+import { useAuth } from "@hooks/useAuth";
 type FormDataProps = {
   name: string;
   email: string;
@@ -41,6 +43,8 @@ const signUpSchema = yup.object({
 });
 
 export function SignUp() {
+  const [isLoading, setIsLoading] = useState(false);
+  const { singIn } = useAuth();
   const toast = useToast();
   const {
     control,
@@ -57,9 +61,12 @@ export function SignUp() {
 
   async function handleSignUp({ name, email, password }: FormDataProps) {
     try {
-      const response = await api.post("/users", { name, email, password });
-      console.log(response.data);
+      setIsLoading(true);
+
+      await api.post("/users", { name, email, password });
+      await singIn(email, password);
     } catch (error) {
+      setIsLoading(false);
       const isAppError = error instanceof AppError;
 
       const title = isAppError
@@ -161,6 +168,7 @@ export function SignUp() {
           <Button
             title="Criar e acessar"
             onPress={handleSubmit(handleSignUp)}
+            isLoading={isLoading}
           />
         </Center>
 
