8da9c7dd90a27b39ff58288d41536608ef8b96a5
wagnermateus <wagnermateus7@gmail.com>
2023-05-04 00:06:27 +0100

    dynamic photo and username

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -2,12 +2,15 @@ import { Heading, HStack, Icon, Text, VStack } from "native-base";
 import { UserPhoto } from "./UserPhoto";
 import { TouchableOpacity } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
+import { useAuth } from "@hooks/useAuth";
+import defaulUserPhotoImg from "@assets/userPhotoDefault.png";
 
 export function HomeHeader() {
+  const { user } = useAuth();
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
       <UserPhoto
-        source={{ uri: "https://github.com/wagnermateus.png" }}
+        source={user.avatar ? { uri: user.avatar } : defaulUserPhotoImg}
         size={16}
         alt="Imagem do usuário"
         mr={4}
@@ -19,7 +22,7 @@ export function HomeHeader() {
         </Text>
 
         <Heading color="gray.100" fontSize="md" fontFamily="heading">
-          Wagner
+          {user.name}
         </Heading>
       </VStack>
 
