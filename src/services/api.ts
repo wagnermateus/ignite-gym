1853e46b06fa49e8f8a550afc462527690597291
wagnermateus <wagnermateus7@gmail.com>
2023-05-04 14:24:17 +0100

    Home-Searching and Listing Groups from the backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import { AppError } from "@utils/AppError";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.1.4:3333",
+  baseURL: "http://192.168.1.8:3333",
 });
 
 api.interceptors.response.use(
