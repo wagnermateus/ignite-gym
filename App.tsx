d310fea9dc1ae0ad062fb033b945fd6ddcde3a90
wagnermateus <wagnermateus7@gmail.com>
2023-07-07 20:31:55 +0100

    adding schemas

--- App.tsx
+++ App.tsx
@@ -9,9 +9,13 @@ import { Loading } from "@components/Loading";
 import { THEME } from "./src/theme";
 import { Routes } from "@routes/index";
 import { AuthContextProvider } from "@contexts/AuthContext";
+import OneSignal from "react-native-onesignal";
+
+OneSignal.setAppId("76675543-8140-4e7c-b2dd-205a2b685525");
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar
